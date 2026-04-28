e0c343f502f4073f78a63273eec8a5f48d47e231
rjnaisu <naismizzle@gmail.com>
2026-04-28 10:32:44 +1000

    feat: entry types to backend

--- patientor/backend/src/types.ts
+++ patientor/backend/src/types.ts
@@ -14,9 +14,6 @@ export const Gender = {
 
 export type Gender = (typeof Gender)[keyof typeof Gender];
 
-//eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface Entry {}
-
 export const NewPatientSchema = z.object({
   name: z.string().trim().min(1, "Name is required"),
   ssn: z.string().trim().min(9, "SSN must be at least 9 characters"),
@@ -42,3 +39,42 @@ export interface Patient {
   entries: Entry[];
 }
 export type PatientNonSensitive = Omit<Patient, "ssn" | "entries">;
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Diagnosis["code"][];
+}
+
+const HealthCheckRating = {
+  Healthy: 0,
+  LowRisk: 1,
+  HighRisk: 2,
+  CriticalRisk: 3,
+} as const;
+
+type HealthCheckRating = (typeof HealthCheckRating)[keyof typeof HealthCheckRating];
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: { date: string; criteria: string };
+}
+
+interface OccupationalHealthCareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: { startDate: string; endDate: string };
+}
+
+export type Entry = HospitalEntry | OccupationalHealthCareEntry | HealthCheckEntry;
+
+type UnionOmit<T, K extends string | number | symbol> = T extends unknown ? Omit<T, K> : never;
+
+export type EntryWithoutId = UnionOmit<Entry, "id">;
